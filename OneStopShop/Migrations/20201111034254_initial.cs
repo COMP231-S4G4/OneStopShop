e8bd06fea099b54aff00625deb5a8dc0204cdeab
Jyothis <jfranc69@my.centennialcollege.ca>
2020-11-12 13:01:24 -0500

    commit for pull

---
+++ OneStopShop/Migrations/20201111034254_initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneStopShop.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    StoreId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StoreName = table.Column<string>(nullable: true),
+                    SellerFirstname = table.Column<string>(nullable: true),
+                    SellerLasttname = table.Column<string>(nullable: true),
+                    StoreDescription = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.StoreId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
