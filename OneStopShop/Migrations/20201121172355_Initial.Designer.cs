2e8f6fcd180ae219664c5a8fd673f86934a7d6c3
Nehal Patel <npate575@my.centennialcollege.ca>
2020-11-21 12:49:18 -0500

    Solved errors after merging master

--- OneStopShop/Migrations/20201121172355_Initial.Designer.cs
+++ OneStopShop/Migrations/20201121172355_Initial.Designer.cs
@@ -10,7 +10,7 @@ using OneStopShop.Models;
 namespace OneStopShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20201121164449_Initial")]
+    [Migration("20201121172355_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
