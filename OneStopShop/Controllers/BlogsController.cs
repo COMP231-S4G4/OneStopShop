bd16c46a00a6619e8692de81d237326f90969b69
Milan <msing926@my.centennialcollege.ca>
2020-11-25 03:30:50 -0500

    Blog function added without linking it with the store id

---
+++ OneStopShop/Controllers/BlogsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OneStopShop.Models;
+
+namespace OneStopShop.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Blogs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BlogId,StoreId,BlogImage,BlogTitle,BlogCreatedDate,BlogModifiedDate,BlogDescription")] Blogs blogs)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blogs);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogs);
+        }
+
+    }
+}
