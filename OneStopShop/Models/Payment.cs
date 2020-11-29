ab3427e0e344e1e648d7624aa4a3c33e63e79a60
Sagar <spate805@my.centennialcollege.ca>
2020-11-29 12:30:23 -0500

    Auto stash before revert of "Error solved and Orders details UI has been done"

---
+++ OneStopShop/Models/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopShop.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int PaymentId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
