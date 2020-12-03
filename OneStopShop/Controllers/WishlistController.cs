8db3883f70b6c8a95b042591796bccd54519876c
Milan <msing926@my.centennialcollege.ca>
2020-12-03 02:22:53 -0500

    Implemented fetching of product added by the buyer in wishlist

--- OneStopShop/Controllers/WishlistController.cs
+++ OneStopShop/Controllers/WishlistController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OneStopShop.Models;
 
 namespace OneStopShop.Controllers
@@ -20,10 +21,19 @@ namespace OneStopShop.Controllers
             _context = context;
         }
 
-        // GET: WishlistController
-        public ActionResult Index()
+        // GET: Wishlist
+        /// <summary>
+        /// This action will get triggered when user/buyer will click on Add to Wishlist button on Product List Page
+        /// This action will pass the product added in the wishlist to the wishlist screen
+        /// Buyer will be able to see all the products in the wishlist
+        /// </summary>
+        /// <returns>Buyer will get all the products that he added in the wishlist</returns>
+
+        public async Task<ActionResult> Index(int productID)
         {
-            return View("WishList");
+            int UserId = (int)HttpContext.Session.GetInt32("UserId");
+            var list = await _context.Wishlists.Where(a => a.UserId.Equals(UserId)).Include(a => a.Product).ToListAsync();
+            return View(list);
         }
 
         // POST: WishlistController/Delete/5
